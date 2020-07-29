5059c9d5fe6224c7b6fb0fac8d211c710a73342b
XNMEPA <magistr00@gmail.com>
2020-07-29 18:07:44 +0300

    JUnit - начало.

---
+++ src/ru/malychev/junit/learning/User.java
@@ -0,0 +1,12 @@
+package ru.malychev.junit.learning;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Sex sex;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
